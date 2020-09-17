439ce38c045d9b58624e3a6ce05369f24aa14b92
PC_Aaron <1021048274@qq.com>
2020-09-17 21:20:29 +0800

    feat:初始化swiper轮播图

--- mini/miniprogram/pages/home/home.js
+++ mini/miniprogram/pages/home/home.js
@@ -5,7 +5,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    imgUrls: [
+      {
+        url: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3669462915,2276960289&fm=26&gp=0.jpg'
+      },
+      {
+        url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1600358438063&di=8ddf2337c756754c2c258002bfe84c01&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F585057cef3127c2098950dd567c6f33d9726b2f4142a3-2mtZpN_fw658'
+      },
+      {
+        url: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2080365821,905282483&fm=26&gp=0.jpg'
+      }
+    ]
   },
 
   /**
