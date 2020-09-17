a45be3968ae6b9ed53c011a0fe5d23f8a2e8873c
PC_Aaron <1021048274@qq.com>
2020-09-17 22:13:54 +0800

    feat:使用observers监听器完成对属性的监听

--- mini/miniprogram/components/playlist/playlist.js
+++ mini/miniprogram/components/playlist/playlist.js
@@ -7,17 +7,38 @@ Component({
     cover: Object
   },
 
+  // 监听器
+  observers: {
+    ['cover.playCount'](count) {
+      console.log(count)
+      this.setData({
+        _count: this._formatNumber(count, 2)
+      })
+    }
+  },
+
   /**
    * 组件的初始数据
    */
   data: {
-
+    _count: 0
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    _formatNumber(num, point){
+      let numStr = num.toString().split('.')[0]
+      if (numStr.length < 6) {
+        return numStr
+      } else if (numStr.length >= 6 && numStr.length <= 8) {
+        let decimal = numStr.substring(numStr.length-4, numStr.length-4 + point)
+        return parseFloat(parseInt(num/10000) + '.' + decimal) + '万'
+      } else if(numStr.length > 8) {
+        let decimal = numStr.substring(numStr.length-8, numStr.length-8 + point)
+        return parseFloat(parseInt(num/100000000) + '.' + decimal) + '亿'
+      }
+    }
   }
 })
