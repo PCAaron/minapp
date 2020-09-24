ba5f71ab706ae62b27e9fb2108e0f952f1eafeaf
PC_Aaron <1021048274@qq.com>
2020-09-24 22:24:24 +0800

    feat:选中音乐并创建音乐播放页

--- mini/miniprogram/components/musicitem/musicitem.js
+++ mini/miniprogram/components/musicitem/musicitem.js
@@ -11,13 +11,21 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    playingid: -1
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onSelect(e){
+      const musicid = e.currentTarget.dataset.musicid
+      this.setData({
+        playingid: musicid
+      })
+      wx.navigateTo({
+        url: `../../pages/player/player?musicid=${musicid}`,
+      })
+    }
   }
 })
