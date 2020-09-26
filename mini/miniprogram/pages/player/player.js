0d1ce10f101f3de0029de8098fb9c967660e0b82
PC_Aaron <1021048274@qq.com>
2020-09-26 21:17:28 +0800

    feat：完成音乐播放功能

--- mini/miniprogram/pages/player/player.js
+++ mini/miniprogram/pages/player/player.js
@@ -1,13 +1,16 @@
 // miniprogram/pages/player/player.js
 let muisclist = []
 let nowPlayingIdx = 0
+// 获取全局唯一的背景音频管理器
+const backgroundAudioManager = wx.getBackgroundAudioManager()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    detail: null
+    detail: null,
+    isPlaying: false // 播放状态
   },
 
   /**
@@ -19,12 +22,12 @@ Page({
     this.loadMuiscDetail()
   },
 
-  async loadMuiscDetail() {
+  loadMuiscDetail() {
+    backgroundAudioManager.stop()
     let music = muisclist[nowPlayingIdx]
+    console.log(music.hash)
     this.formatTile(music.filename)
-    wx.setNavigationBarTitle({
-      title: music.filename
-    })
+    wx.showLoading()
     wx.cloud.callFunction({
       name: 'music',
       data: {
@@ -33,8 +36,21 @@ Page({
       }
     }).then(res => {
       this.setData({
-        detail: res.result.data
+        detail: res.result.data,
+        isPlaying: false
       })
+      wx.setNavigationBarTitle({
+        title: this.data.detail.song_name
+      })
+      backgroundAudioManager.src = this.data.detail.play_url
+      backgroundAudioManager.title = this.data.detail.song_name
+      backgroundAudioManager.singer = this.data.detail.author_name
+      backgroundAudioManager.coverImgUrl = this.data.detail.img
+      backgroundAudioManager.play()
+      this.setData({
+        isPlaying: true
+      })
+      wx.hideLoading()
     })
   },
 
@@ -42,6 +58,32 @@ Page({
     const opts = filename.indexOf('-')
     console.log(opts)
   }, 
+
+  togglePlay() {
+    if(this.data.isPlaying){
+      backgroundAudioManager.pause()
+    } else {
+      backgroundAudioManager.play()
+    }
+    this.setData({
+      isPlaying: !this.data.isPlaying
+    })
+  },
+
+  onPrev() {
+    nowPlayingIdx--
+    if(nowPlayingIdx < 0){
+      nowPlayingIdx = muisclist.length -1
+    } 
+    this.loadMuiscDetail()
+  },
+  onNext() {
+    nowPlayingIdx++
+    if(nowPlayingIdx === muisclist.length){
+      nowPlayingIdx = 0
+    } 
+    this.loadMuiscDetail()
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
