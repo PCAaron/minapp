e26c66ae6527a9d8440e9be91378554e5ec4aacf
PC_Aaron <1021048274@qq.com>
2020-09-29 22:35:38 +0800

    feat:完成发布博客布局

--- mini/miniprogram/pages/blog/blog.js
+++ mini/miniprogram/pages/blog/blog.js
@@ -1,11 +1,13 @@
 // miniprogram/pages/blog/blog.js
+const MAX_WORD_NUM = 140 //文字最大长度
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    wordNum: 0, //输入文字个数
+    footerBottom: 0
   },
 
   /**
@@ -15,6 +17,26 @@ Page({
     console.log(options)
   },
 
+  onInput(e) {
+    let wordNum = e.detail.value.length
+    if(wordNum >= MAX_WORD_NUM) {
+      wordNum = `最大字数为${MAX_WORD_NUM}`
+    } else {
+      this.setData({
+        wordNum
+      })
+    }
+  },
+  onFocus(e) {
+    this.setData({
+      footerBottom: e.detail.height // 虚拟键盘
+    })
+  },
+  onBlur() {
+    this.setData({
+      footerBottom: 0
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
