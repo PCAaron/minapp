010a48f77f2bf5b6c82fdf1d8adde74b704647ca
PC_Aaron <1021048274@qq.com>
2020-10-13 22:29:17 +0800

    feat:完成评论功能

--- mini/miniprogram/components/blogcoment/blogcoment.js
+++ mini/miniprogram/components/blogcoment/blogcoment.js
@@ -1,11 +1,12 @@
 // components/blogcoment/blogcoment.js
 let userInfo = ''
+const db = wx.cloud.database()
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
-
+    blogId: String
   },
   externalClasses: ['iconfont','icon-pinglun','icon-fenxiang'],
   /**
@@ -43,7 +44,8 @@ Component({
         }
       })
     },
-    onloginSuccess() {
+    onloginSuccess(e) {
+      userInfo = e.detail
       this.setData({
         loginShow: false,
       },() => {
@@ -56,6 +58,45 @@ Component({
       wx.showModal({
         content:'授权用户才能进行评论'
       })
+    },
+    onInput(e){
+      this.setData({
+        content: e.detail.value
+      })
+    },
+    onSend() {
+      // 插入数据库
+      let content = this.data.content
+      if(content.trim() == ''){
+        wx.showModal({
+          content: '评论内容不能为空'
+        })
+        return
+      }
+      wx.showLoading({
+        title: '评论中',
+        mask: true
+      })
+      db.collection('blog-comment').add({
+        data: {
+          content,
+          createTime: db.serverDate(),
+          blogId:this.properties.blogId,
+          nickName: userInfo.nickName,
+          avatarUrl: userInfo.avatarUrl
+        }
+      }).then(res=>{
+        wx.hideLoading()
+        wx.showToast({
+          title:'评论成功'
+        })
+        this.setData({
+          modalShow: false,
+          content: ''
+        })
+      })
+
+      // 推送模板消息
     }
   }
 })
